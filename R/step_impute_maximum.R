accbfb9f0df6b455fc69fa0164ef8662ac36e16b
mwgard <43005024+mwgard@users.noreply.github.com>
2024-05-28 11:21:17 -0500

    Added scale_impute_maximum function.

--- R/step_impute_maximum.R
+++ R/step_impute_maximum.R
@@ -1,43 +1,17 @@
-cast2 <- function (x, ref) {
-  if (is.factor(ref)) {
-    x <- factor(x, levels = levels(ref), ordered = is.ordered(ref))
-  }
-  else {
-    if (is.integer(ref) & !is.factor(ref)) {
-      x <- as.integer(round(x, 0))
-    }
-  }
-  x
-}
-
-weighted_median_impl2 <- function (x, wts)  {
-  order_x <- order(x)
-  x <- x[order_x]
-  wts <- wts[order_x]
-  wts_norm <- cumsum(wts)/sum(wts)
-  ps <- min(which(wts_norm > 0.5))
-  x[ps]
-}
-
-cov2pca2 <- function (cv_mat) {
-  res <- eigen(cv_mat)
-  list(sdev = sqrt(res$values), rotation = res$vectors)
-}
-
 #' @importFrom purrr map_dbl
-minimums <- function(x, wts = NULL) {
+maximums <- function(x, wts = NULL) {
   if (NCOL(x) == 0) {
-    return(vapply(x, min, c(min = 0), na.rm = TRUE))
+    return(vapply(x, max, c(max = 0), na.rm = TRUE))
   }
-  res <- apply(x, 2, min, na.rm = TRUE)
+  res <- apply(x, 2, max, na.rm = TRUE)
   res
 }
 
 
-#' Impute numeric data using the minimum (or half-minimum)
+#' Impute numeric data using the maximum (or factor * maximum)
 #'
-#' `step_impute_minimum()` creates a *specification* of a recipe step that will
-#' substitute missing values of numeric variables by the training set minimum of
+#' `step_impute_maximum()` creates a *specification* of a recipe step that will
+#' substitute missing values of numeric variables by the training set maximum of
 #' those variables.
 #'
 #' @param recipe A recipe object. The step will be added to the sequence
@@ -46,17 +20,12 @@ minimums <- function(x, wts = NULL) {
 #' @param role Not used by this step since no new variables are created.
 #' @param trained A logical to indicate if the quantities for preprocessing have
 #' been estimated.
-#' @param minimums A named numeric vector of minimums. This is `NULL` until
+#' @param maximums A named numeric vector of maximums. This is `NULL` until
 #'  computed by [prep()]. Note that, if the original data are integers,
-#'  the minimum will be converted to an integer to maintain the same data type.
-#' @param factor A numeric value greater than 0 and less than or equal to 1 to
-#' scale the minimum values. To replace values with the half-minimum, set this
-#' argument to 0.5. Default value of 1.
-#' @param rand_value Logical value (`TRUE` or `FALSE` (default)) as to whether
-#' a random uniform value greater than 0 and less than the minimum should be
-#' used for NA replacement. Note that the random value is set for all `NA`
-#' values for each numeric column being imputed, i.e., each `NA` does not get
-#' its own random value. If set to FALSE, the minimum value is used.
+#'  the maximum will be converted to an integer to maintain the same data type.
+#' @param factor A numeric value greater than or equal to 1 to scale the maximum
+#' values. To replace values with the double-maximum, set this argument to 2.
+#' Default value of 1.
 #' @param skip A logical. Should the step be skipped when the recipe is baked by
 #' bake()? While all operations are baked when prep() is run, some operations
 #' may not be able to be conducted on new data (e.g. processing the outcome
@@ -68,16 +37,16 @@ minimums <- function(x, wts = NULL) {
 #' @importFrom recipes printer check_type is_trained sel2char ellipse_check
 #' @importFrom generics tidy required_pkgs
 #' @importFrom rlang enquos
-#' @details `step_impute_minimum` estimates the variable minimums from the data
+#' @details `step_impute_maximum` estimates the variable maximums from the data
 #'  used in the `training` argument of `prep.recipe`. `bake.recipe` then applies
-#'  the new values to new data sets using these minimums.
+#'  the new values to new data sets using these maximums.
 #'
 #'
 #' # Tidying
 #'
 #' When you [`tidy()`][tidy.recipe()] this step, a tibble with
 #' columns `terms` (the selectors or variables selected) and `model`
-#' (the minimum value) is returned.
+#' (the maximum value) is returned.
 #'
 #' @examplesIf rlang::is_installed("modeldata")
 #' data("credit_data", package = "modeldata")
@@ -95,7 +64,7 @@ minimums <- function(x, wts = NULL) {
 #' rec <- recipe(Price ~ ., data = credit_tr)
 #'
 #' impute_rec <- rec %>%
-#'   step_impute_minimum(Income, Assets, Debt)
+#'   step_impute_maximum(Income, Assets, Debt)
 #'
 #' imp_models <- prep(impute_rec, training = credit_tr)
 #'
@@ -106,25 +75,23 @@ minimums <- function(x, wts = NULL) {
 #'
 #' tidy(impute_rec, number = 1)
 #' tidy(imp_models, number = 1)
-step_impute_minimum <-
+step_impute_maximum <-
   function(recipe,
            ...,
            role = NA,
            trained = FALSE,
-           minimums = NULL,
+           maximums = NULL,
            factor = 1,
-           rand_value = FALSE,
            skip = FALSE,
-           id = recipes::rand_id("impute_minimum")) {
+           id = recipes::rand_id("impute_maximum")) {
     add_step(
       recipe,
-      step_impute_minimum_new(
+      step_impute_maximum_new(
         terms = rlang::enquos(...),
         role = role,
         trained = trained,
-        minimums = minimums,
+        maximums = maximums,
         factor = factor,
-        rand_value = rand_value,
         skip = skip,
         id = id,
         case_weights = NULL
@@ -133,16 +100,15 @@ step_impute_minimum <-
   }
 
 #' @importFrom recipes step
-step_impute_minimum_new <-
-  function(terms, role, trained, minimums, factor, rand_value, skip, id, case_weights) {
+step_impute_maximum_new <-
+  function(terms, role, trained, maximums, factor, skip, id, case_weights) {
     step(
-      subclass = "impute_minimum",
+      subclass = "impute_maximum",
       terms = terms,
       role = role,
       trained = trained,
-      minimums = minimums,
+      maximums = maximums,
       factor = factor,
-      rand_value = rand_value,
       skip = skip,
       id = id,
       case_weights = case_weights
@@ -155,7 +121,7 @@ step_impute_minimum_new <-
 #' @importFrom rlang warn
 #' @importFrom stats runif
 #' @export
-prep.step_impute_minimum <- function(x, training, info = NULL, ...) {
+prep.step_impute_maximum <- function(x, training, info = NULL, ...) {
   col_names <- recipes_eval_select(x$terms, training, info)
   check_type(training[, col_names], types = c("double", "integer"))
 
@@ -166,35 +132,23 @@ prep.step_impute_minimum <- function(x, training, info = NULL, ...) {
   # }
   were_weights_used <- FALSE
   wts <- NULL
-  if (x$factor <= 0 | x$factor > 1) {
-    rlang::warn(paste0("Scaling `factor` should take either a value greater ",
-                       "than 0 and less than 1. Setting to 1."))
+  if (x$factor < 1) {
+    rlang::warn(paste0("Scaling `factor` should take a value greater ",
+                       "than or equal to 1. Setting to 1."))
     x$factor <- 1
   }
 
-  minimums <- minimums(training[, col_names], wts = wts)
-  minimums <- minimums * x$factor
-  if (!is.logical(x$rand_value)) {
-    rlang::warn("`rand_value` must be FALSE or TRUE. Setting to FALSE")
-    x$rand_value <- FALSE
-  }
-  if (x$rand_value == TRUE) {
-    nm <- names(minimums)
-    minimums <- stats::runif(length(minimums),
-                             min = rep(0, length(minimums)),
-                             max = minimums)
-    names(minimums) <- nm
-  }
+  maximums <- maximums(training[, col_names], wts = wts)
+  maximums <- maximums * x$factor
 
-  minimums <- purrr::map2(minimums, training[, col_names], cast2)
+  maximums <- purrr::map2(maximums, training[, col_names], cast2)
 
-  step_impute_minimum_new(
+  step_impute_maximum_new(
     terms = x$terms,
     role = x$role,
     trained = TRUE,
-    minimums = minimums,
+    maximums = maximums,
     factor = x$factor,
-    rand_value = x$rand_value,
     skip = x$skip,
     id = x$id,
     case_weights = were_weights_used
@@ -204,41 +158,41 @@ prep.step_impute_minimum <- function(x, training, info = NULL, ...) {
 #' @importFrom recipes check_new_data bake
 #' @importFrom vctrs vec_cast
 #' @export
-bake.step_impute_minimum <- function(object, new_data, ...) {
-  check_new_data(names(object$minimums), object, new_data)
+bake.step_impute_maximum <- function(object, new_data, ...) {
+  check_new_data(names(object$maximums), object, new_data)
 
-  for (i in names(object$minimums)) {
+  for (i in names(object$maximums)) {
     if (any(is.na(new_data[[i]]))) {
-      new_data[[i]] <- vctrs::vec_cast(new_data[[i]], object$minimums[[i]])
+      new_data[[i]] <- vctrs::vec_cast(new_data[[i]], object$maximums[[i]])
     }
-    new_data[is.na(new_data[[i]]), i] <- object$minimums[[i]]
+    new_data[is.na(new_data[[i]]), i] <- object$maximums[[i]]
   }
   new_data
 }
 
 #' @importFrom recipes print_step
 #' @export
-print.step_impute_minimum <-
+print.step_impute_maximum <-
   function(x, width = max(20, options()$width - 30), ...) {
-    title <- "minimum imputation for "
-    print_step(names(x$minimums), x$terms, x$trained, title, width,
+    title <- "maximum imputation for "
+    print_step(names(x$maximums), x$terms, x$trained, title, width,
                case_weights = x$case_weights)
     invisible(x)
   }
 
-#' @rdname step_impute_minimum
-#' @param x A `step_impute_minimum` object.
+#' @rdname step_impute_maximum
+#' @param x A `step_impute_maximum` object.
 #' @importFrom recipes sel2char is_trained
 #' @importFrom tibble tibble
 #' @importFrom rlang na_dbl
 #' @importFrom generics tidy
 #' @importFrom vctrs list_unchop
 #' @export
-tidy.step_impute_minimum <- function(x, ...) {
+tidy.step_impute_maximum <- function(x, ...) {
   if (is_trained(x)) {
     res <- tibble::tibble(
-      terms = names(x$minimums),
-      value = vctrs::list_unchop(unname(x$minimums), ptype = double())
+      terms = names(x$maximums),
+      value = vctrs::list_unchop(unname(x$maximums), ptype = double())
     )
   } else {
     term_names <- sel2char(x$terms)
@@ -250,6 +204,6 @@ tidy.step_impute_minimum <- function(x, ...) {
 
 #' @export
 #' @importFrom generics required_pkgs
-required_pkgs.step_impute_minimum <- function(x, ...) {
+required_pkgs.step_impute_maximum <- function(x, ...) {
   c("scaleParetoImputeMin")
 }
